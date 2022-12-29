4210e551fd82330d8e2610a43965e4623ec0bacf
Vinnie Magro <v@vinn.ie>
2022-12-29 16:34:42 -0500

    btrfs: receive sendstream starts but no contents

--- src/btrfs.rs
+++ src/btrfs.rs
@@ -1,84 +1,60 @@
-use std::borrow::Cow;
-use std::os::unix::fs::PermissionsExt;
+use std::collections::BTreeMap;
 
 use sendstream_parser::Command;
-use sendstream_parser::Error;
 use sendstream_parser::Sendstream;
+use uuid::Uuid;
 
-use crate::entry::Directory;
-use crate::File;
-use crate::Filesystem;
-
-pub struct Sendstreams<'s> {
-    sendstreams: Vec<Sendstream<'s>>,
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("invariant violated: {0}")]
+    InvariantViolated(&'static str),
+    #[error("parent subvol not yet received: {0}")]
+    MissingParent(Uuid),
+    #[error(transparent)]
+    Parse(#[from] sendstream_parser::Error),
 }
 
-impl<'s> Sendstreams<'s> {
-    pub fn new(sendstreams: Vec<Sendstream<'s>>) -> Self {
-        Self { sendstreams }
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Subvol {
+    parent_uuid: Option<Uuid>,
+}
+
+impl Subvol {
+    fn new() -> Self {
+        Subvol { parent_uuid: None }
     }
 }
 
-macro_rules! subvol_path {
-    ($subvol:ident, $path:expr) => {
-        Cow::Owned($subvol.path().join($path))
-    };
-}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Subvols(BTreeMap<Uuid, Subvol>);
 
-impl<'s> Sendstreams<'s> {
-    pub fn filesystem(&self) -> Result<Filesystem<'_, '_>, Error> {
-        let mut fs = Filesystem::new();
-        for sendstream in &self.sendstreams {
-            let mut cmd_iter = sendstream.commands().iter();
-            let subvol = match cmd_iter.next().expect("must have at least one command") {
-                Command::Subvol(s) => s,
-                _ => panic!("first command is always the subvolume"),
-            };
-            fs.entries
-                .insert(subvol.path().clone(), Directory::default().into());
-            for command in cmd_iter {
-                eprintln!("{command:?}");
-                match command {
-                    Command::Chown(c) => {
-                        fs.entries
-                            .get_mut(&subvol_path!(subvol, c.path()))
-                            .expect("must exist")
-                            .chown(c.uid(), c.gid());
-                    }
-                    Command::Chmod(c) => fs
-                        .entries
-                        .get_mut(&subvol_path!(subvol, c.path()))
-                        .expect("must exist")
-                        .chmod(nix::sys::stat::Mode::from_bits_truncate(
-                            c.mode().permissions().mode(),
-                        )),
-                    Command::Mkdir(m) => {
-                        fs.entries.insert(
-                            subvol_path!(subvol, m.path().path()),
-                            Directory::default().into(),
-                        );
-                    }
-                    Command::Mkfile(m) => {
-                        fs.entries.insert(
-                            subvol_path!(subvol, m.path().path()),
-                            File::default().into(),
-                        );
-                    }
-                    Command::Rename(r) => {
-                        let from = fs
-                            .entries
-                            .remove(&subvol_path!(subvol, r.from()))
-                            .expect("rename from must exist");
-                        fs.entries.insert(subvol_path!(subvol, r.to()), from);
-                    }
-                    // Command::Mkfile(m) => {
+impl Subvols {
+    pub fn new() -> Self {
+        Self(BTreeMap::new())
+    }
 
-                    // }
-                    _ => {}
-                }
+    /// Parse subvolumes from an uncompressed sendstream
+    pub fn receive<'f>(&mut self, sendstream: Sendstream<'f>) -> Result<(), Error> {
+        let mut cmd_iter = sendstream.commands().iter();
+        let (subvol_uuid, subvol) = match cmd_iter.next().expect("must have at least one command") {
+            Command::Subvol(s) => (s.uuid(), Subvol::new()),
+            Command::Snapshot(s) => {
+                let mut subvol = self
+                    .0
+                    .get(&s.clone_uuid())
+                    .ok_or(Error::MissingParent(s.clone_uuid()))?
+                    .clone();
+                subvol.parent_uuid = Some(s.clone_uuid());
+                (s.uuid(), subvol)
             }
-        }
-        Ok(fs)
+            _ => {
+                return Err(Error::InvariantViolated(
+                    "first command was not subvol start",
+                ))
+            }
+        };
+        self.0.insert(subvol_uuid, subvol);
+        Ok(())
     }
 }
 
@@ -86,21 +62,39 @@ impl<'s> Sendstreams<'s> {
 mod tests {
     use std::path::Path;
 
+    use bytes::Bytes;
     use pretty_assertions::assert_eq;
+    use uuid::uuid;
 
     use super::*;
-    use crate::tests::demo_fs;
 
     #[test]
     fn sendstream() {
-        let file = std::fs::File::open(Path::new(env!("OUT_DIR")).join("testdata.sendstream"))
-            .expect("failed to open testdata.sendstream");
-        let contents =
-            unsafe { memmap::MmapOptions::new().map(&file) }.expect("failed to mmap sendstream");
+        let contents = Bytes::from(
+            std::fs::read(Path::new(env!("OUT_DIR")).join("testdata.sendstream"))
+                .expect("failed to read testdata.sendstream"),
+        );
         let sendstreams = Sendstream::parse_all(&contents).expect("failed to parse sendstream");
-        let sendstreams = Sendstreams::new(sendstreams);
-        let fs = sendstreams.filesystem().expect("failed to create fs");
-        let mut demo_fs = demo_fs();
-        assert_eq!(fs, demo_fs);
+        let mut subvols = Subvols::new();
+        for sendstream in sendstreams {
+            subvols
+                .receive(sendstream)
+                .expect("failed to receive sendstream");
+        }
+        assert_eq!(
+            BTreeMap::from([
+                (
+                    uuid!("0fbf2b5f-ff82-a748-8b41-e35aec190b49"),
+                    Subvol { parent_uuid: None }
+                ),
+                (
+                    uuid!("ed2c87d3-12e3-c549-a699-635de66d6f35"),
+                    Subvol {
+                        parent_uuid: Some(uuid!("0fbf2b5f-ff82-a748-8b41-e35aec190b49")),
+                    }
+                )
+            ]),
+            subvols.0
+        );
     }
 }
