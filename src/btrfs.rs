37ab807b8390c1128011bf30824f0a46994448d1
Vinnie Magro <v@vinn.ie>
2023-01-11 15:50:37 -0500

    btrfs: better handling of mixed subvol/snapshot commands

--- src/btrfs.rs
+++ src/btrfs.rs
@@ -3,7 +3,6 @@ use std::io::Seek;
 use std::io::SeekFrom;
 use std::ops::Deref;
 
-use anyhow::anyhow;
 use bytes::Bytes;
 use sendstream_parser::Command;
 use sendstream_parser::Sendstream;
@@ -30,6 +29,23 @@ pub enum Error<'c> {
     },
 }
 
+enum ApplyError<'c> {
+    Apply(crate::Error),
+    Btrfs(Error<'c>),
+}
+
+impl<'c> From<crate::Error> for ApplyError<'c> {
+    fn from(e: crate::Error) -> Self {
+        Self::Apply(e)
+    }
+}
+
+impl<'c> From<Error<'c>> for ApplyError<'c> {
+    fn from(e: Error<'c>) -> Self {
+        Self::Btrfs(e)
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Subvol {
     parent_uuid: Option<Uuid>,
@@ -54,11 +70,20 @@ impl Subvols {
     }
 
     #[remain::check]
-    fn apply_cmd(&mut self, subvol: &mut Subvol, cmd: &Command<'_>) -> crate::Result<()> {
-        #[remain::sorted]
+    fn apply_cmd<'c>(
+        &mut self,
+        subvol: &mut Subvol,
+        cmd: &Command<'c>,
+    ) -> Result<(), ApplyError<'c>> {
         match cmd {
-            Command::Chmod(c) => subvol.fs.chmod(c.path(), c.mode().mode()),
-            Command::Chown(c) => subvol.fs.chown(c.path(), c.uid(), c.gid()),
+            Command::Chmod(c) => {
+                subvol.fs.chmod(c.path(), c.mode().mode())?;
+                Ok(())
+            }
+            Command::Chown(c) => {
+                subvol.fs.chown(c.path(), c.uid(), c.gid())?;
+                Ok(())
+            }
             Command::Clone(c) => {
                 let src = subvol.fs.get_file(c.src_path())?;
                 let start = c.src_offset().as_u64();
@@ -73,7 +98,10 @@ impl Subvols {
                 Ok(())
             }
             Command::End => Ok(()),
-            Command::Link(l) => subvol.fs.link(l.target().as_path(), l.link_name()),
+            Command::Link(l) => {
+                subvol.fs.link(l.target().as_path(), l.link_name())?;
+                Ok(())
+            }
             Command::Mkdir(m) => {
                 subvol.fs.insert(m.path().as_path(), Directory::default());
                 Ok(())
@@ -112,8 +140,15 @@ impl Subvols {
                     .retain(|k, _| k != r.name().deref());
                 Ok(())
             }
-            Command::Rename(r) => subvol.fs.rename(r.from(), r.to()),
-            Command::Rmdir(r) => subvol.fs.rmdir(r.path()),
+            Command::Rename(r) => {
+                subvol.fs.rename(r.from(), r.to())?;
+                Ok(())
+            }
+
+            Command::Rmdir(r) => {
+                subvol.fs.rmdir(r.path())?;
+                Ok(())
+            }
             Command::SetXattr(s) => {
                 subvol.fs.get_mut(s.path())?.metadata_mut().xattrs.insert(
                     Bytes::copy_from_slice(s.name()),
@@ -121,26 +156,33 @@ impl Subvols {
                 );
                 Ok(())
             }
-            Command::Snapshot(s) => {
-                todo!()
+            Command::Snapshot(_) => {
+                Err(Error::InvariantViolated("attempted to apply snapshot").into())
             }
-            Command::Subvol(s) => {
-                todo!()
-            }
+            Command::Subvol(_) => Err(Error::InvariantViolated("attempted to apply subvol").into()),
             Command::Symlink(s) => {
                 subvol
                     .fs
                     .insert(s.link_name(), Symlink::new(s.target().as_path(), None));
                 Ok(())
             }
-            Command::Truncate(t) => subvol.fs.truncate(t.path(), t.size()),
-            Command::Unlink(u) => subvol.fs.unlink(u.path()),
-            Command::UpdateExtent(_) => {
-                Err(anyhow!("UpdateExtent command is not supported").into())
+            Command::Truncate(t) => {
+                subvol.fs.truncate(t.path(), t.size())?;
+                Ok(())
             }
-            Command::Utimes(u) => subvol
-                .fs
-                .set_times(u.path(), *u.ctime(), *u.atime(), *u.mtime()),
+            Command::Unlink(u) => {
+                subvol.fs.unlink(u.path())?;
+                Ok(())
+            }
+            Command::UpdateExtent(_) => {
+                Err(Error::InvariantViolated("UpdateExtent command is not supported").into())
+            }
+            Command::Utimes(u) => {
+                subvol
+                    .fs
+                    .set_times(u.path(), *u.ctime(), *u.atime(), *u.mtime())?;
+                Ok(())
+            }
             Command::Write(w) => {
                 let f = subvol.fs.get_file_mut(w.path())?;
                 let mut wr = f.writer();
@@ -155,7 +197,7 @@ impl Subvols {
     /// Parse subvolumes from an uncompressed sendstream
     pub fn receive<'f>(&mut self, sendstream: Sendstream<'f>) -> Result<(), Error<'f>> {
         let mut cmd_iter = sendstream.into_commands().into_iter();
-        let (subvol_uuid, mut subvol) = #[remain::sorted]
+        let (mut subvol_uuid, mut subvol) = #[remain::sorted]
         match cmd_iter
             .next()
             .expect("must have at least one command")
@@ -177,11 +219,34 @@ impl Subvols {
             _ => return Err(Error::InvariantViolated("first command was not subvol start").into()),
         };
         for cmd in cmd_iter {
-            self.apply_cmd(&mut subvol, &cmd)
-                .map_err(|error| Error::Apply {
-                    command: cmd,
-                    error,
-                })?;
+            match &cmd {
+                Command::Snapshot(s) => {
+                    self.0.insert(subvol_uuid, subvol);
+                    subvol = self
+                        .0
+                        .get(&s.clone_uuid())
+                        .ok_or(Error::MissingParent(s.clone_uuid()))?
+                        .clone();
+                    subvol.parent_uuid = Some(s.clone_uuid());
+                    subvol_uuid = s.uuid();
+                }
+                Command::Subvol(s) => {
+                    self.0.insert(subvol_uuid, subvol);
+                    subvol = Subvol::new();
+                    subvol.fs.insert("", Directory::default());
+                    subvol_uuid = s.uuid();
+                }
+                _ => {
+                    self.apply_cmd(&mut subvol, &cmd)
+                        .map_err(|error| match error {
+                            ApplyError::Apply(error) => Error::Apply {
+                                command: cmd,
+                                error,
+                            },
+                            ApplyError::Btrfs(error) => error,
+                        })?;
+                }
+            }
         }
         self.0.insert(subvol_uuid, subvol);
         Ok(())
